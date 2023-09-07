e8d15700271b092ebabc7b0589e2ea81bfb2d67d
Crosson Brieuc <crosson.brieuc@gmail.com>
2023-09-07 18:21:57 +0200

    makefile ''working'' ; LoadImage ; .h files in includesH

---
+++ includesH/ImageProcessing/LoadImage.h
@@ -0,0 +1,6 @@
+#ifndef LoadImage
+#define LoadImage
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer);
+
+#endif
---
+++ src/ImageProcessing/LoadImage.c
@@ -0,0 +1,24 @@
+#include <SDL2/SDL.h>
+
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer)
+{
+    SDL_Surface *tmp = NULL; 
+    SDL_Texture *texture = NULL;
+    tmp = SDL_LoadBMP(path);
+    if(NULL == tmp)
+    {
+        fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
+        return NULL;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, tmp);
+    SDL_FreeSurface(tmp);
+    if(NULL == texture)
+    {
+        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
+        return NULL;
+    }
+    return texture;
+}
+
+
