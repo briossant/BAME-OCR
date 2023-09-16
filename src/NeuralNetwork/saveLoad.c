d450ab75129c9a55640f1f92797ecfd728f71321
briossant <crosson.brieuc@gmail.com>
2023-09-16 14:27:39 +0200

    comments & quick fixes in loadNetwork -> function isn't finished

--- src/NeuralNetwork/saveLoad.c
+++ src/NeuralNetwork/saveLoad.c
@@ -36,7 +36,11 @@ void SaveNetwork(Network network, char* filepath)
 
 }
 
-double* get_numbers(const char* input, int* count) {
+int isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+double* get_numbers(const char* input, int* count) { // I think *count isn't needed as an argument
     double* numbers = NULL;
     *count = 0;
     double currentNumber = 0.0;
@@ -44,8 +48,8 @@ double* get_numbers(const char* input, int* count) {
     int isParsingDecimal = 0;
     double decimalMultiplier = 0.1;
 
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (isdigit(input[i])) {  //verify what libray is this and fix
+    for (int i = 0; input[i] != '\0'; i++) { 
+        if (isDigit(input[i])) { // isDigit done  
             if (isParsingDecimal) {
                 currentNumber = currentNumber + (input[i] - '0') * decimalMultiplier;
                 decimalMultiplier /= 10.0;
@@ -81,69 +85,77 @@ double* get_numbers(const char* input, int* count) {
 Network LoadNetwork(char *filepath)
 {
 
-   FILE * file= fopen( filepath, "r"); // this will clear the file so we can write on it, if we don't want the 
+    FILE * file= fopen( filepath, "r"); // this will clear the file so we can write on it, if we don't want the 
                                        // file to be cleared do: fopen(filepath,"a" for appending
-   //Initialize a Network
-   //new Network net = Network(0,0)
-   if (file == NULL)
-   {
-       printf ("Error this file does not exist or can't be opened\n");
-       exit(1);
-   }
-   int l=0;
-   if (fgets(line,l, file)!= NULL)
-   {
-       char* line = fgets(line,0, file);
-       double* numbers = get_numbers(line);
-        // input_size * depth layer 
-        // D fois 
-        // layer size L
-        // node l fois
-       //remember to free(numbers) every single time
-       if ( sizeof(numbers) < 2)
-       {
-           printf ("there was no size or depth for the network");
-           exit(1);
-           //or do we take the usual values?
-       }
-       Network network = new Network(numbers[1], numbers[0]);  //verify
-       l++;
-       size_t D= numbers[1];
-       while ( D>0 && fgets(line, l, file)!= NULL)
-       {
-            
-            char* line = fgets(line,l, file);
-            double* numbers = get_numbers(line);
-            // new layer put layer size 
-            size_t L= numbers[0];
-            while (L>0 && fgets(line, l, file)!= NULL)
-            {
-                char* line = fgets(line,0, file);
-                double* numbers = get_numbers(line);
-                //create a node and put the weights
-                L--;
-                l++;
-            }
+    //Initialize a Network
+    //new Network net = Network(0,0)
+    if (file == NULL)
+    {
+        printf ("Error this file does not exist or can't be opened\n");
+        exit(1);
+    }
+    int l=0;
+   
+    char* line = fgets(line,0, file);
 
-            if (L!=0)
-            {
-                printf ("the file did not have all the nod's info, stoped at %ld node, %ld layer", L, D);
-                exit(1);
-            }
-            D--;
-            l++;
-       }
-       if (D!=0)
-       {
-           printf( "the file did not have all the Layers, stoped at %ld layer",D); 
-           exit(1);
-       }
+    if (line != NULL) { // line is undeclared
+        printf("The file was empty\n");
+        exit(1); // what is the default network?? there's no default just throw an error 
+    }       
+
+    double* numbers = get_numbers(line);
+    // input_size * depth layer 
+    // D fois 
+    // layer size L
+    // node l fois
+    //remember to free(numbers) every single time
+    if ( sizeof(numbers) < 2)
+    {
+        printf ("there was no size or depth for the network\n"); // don't forget \n at the end of printf
+        exit(1);
+        //or do we take the usual values? there isn't usual values
+    }
     
-   }
-   else {
-    return Network(0,0); // what is the default network??
-    printf(" The default Network was Loaded");
-   }
+    // this is how you create a new struct : 
+    Layer *layers = malloc(sizeof(Layer) * numbers[0]); // the array needs to be created before
+    Network network = { // this is the new Network declaration
+        .input_size = numbers[1], 
+        .depth = numbers[0],
+        .layers = layers
+    };  
+    
 
+    l++;
+    size_t D = numbers[1];
+    while ( D>0 && fgets(line, l, file)!= NULL)
+    {
+        char* line = fgets(line,l, file);
+        double* numbers = get_numbers(line);
+        // new layer put layer size 
+        size_t L= numbers[0];
+        while (L>0 && fgets(line, l, file)!= NULL)
+        {
+            char* line = fgets(line,0, file);
+            double* numbers = get_numbers(line);
+            //create a node and put the weights
+            L--;
+            l++;
+        }
+
+        if (L!=0)
+        {
+            printf ("the file did not have all the nod's info, stoped at %ld node, %ld layeri\n", L, D);
+            exit(1);
+        }
+        D--;
+        l++;
+    }
+    if (D!=0)
+    {
+        printf( "the file did not have all the Layers, stoped at %ld layer\n",D); 
+        exit(1);
+    }
+    
+    return network;
 }
 
