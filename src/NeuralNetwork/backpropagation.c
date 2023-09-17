85b394b6d5fdd883e538f2d54cbae27791b1290c
briossant <crosson.brieuc@gmail.com>
2023-09-17 16:19:49 +0200

    UpdateLayer() done -> updates weights and biases according to *DCost

--- src/NeuralNetwork/backpropagation.c
+++ src/NeuralNetwork/backpropagation.c
@@ -25,28 +25,46 @@ double *firstDCost(double *outputActivations, double *expectedOutputs, size_t ou
 }
 
 
+void UpdateLayer(Layer layer, double* DCost) {
+    for(size_t i=0;i<layer.size;i++) {
+        layer.nodes[i].bias -= DCost[i] * TRAINING_RATE;
+        for(size_t j=0;j<layer.nodes[i].weight_size;j++) {
+            layer.nodes[i].weights[j] -= DCost[i] * TRAINING_RATE;    
+        }    
+    }
+}
+
+
 void BackPropagation(Network network,  double **input_batch, double **output_batch, size_t batch_size) {
     for (size_t m = 0; m < batch_size;m++) {
         double **activationsLayers = PropagateAndKeep(input_batch[m], network);
         
         size_t outputLayerI = network.depth-1;
-        double *lastDCost = firstDCost(activationsLayers[outputLayerI], output_batch[m], network.layers[outputLayerI].size);
+        double *DCost = firstDCost(activationsLayers[outputLayerI], output_batch[m], network.layers[outputLayerI].size);
         
         // update first weights and biases
+        UpdateLayer(network.layers[outputLayerI], DCost);
 
         for(size_t l = outputLayerI-1; l >= 0; l--) {
             
+            double *newDCost = malloc(network.layers[l].size * sizeof(double));
             for (size_t i=0; i<network.layers[l].size;i++){
-                // calculate new DCost 
+                // calculate newDCost 
                 // start by calculating sig'(z[i])
                 for (size_t j=0; j<network.layers[l+1].size;j++){
-                    // DCost[i] += (layer[j] -> weight[i]) * sig'(z[i]) * lastDCost[j]
+                    // newDCost[i] += (layer[j] -> weight[i]) * sig'(z[i]) * DCost[j]
                 }
-
-                // update layer l weights & biases according to new DCost
+                
             }
+
+            free(DCost);
+            DCost = newDCost;
+
+            // update layer l weights & biases according to new DCost
+            UpdateLayer(network.layers[l], DCost);
         }
 
+        free(DCost);
     }         
 }
 
