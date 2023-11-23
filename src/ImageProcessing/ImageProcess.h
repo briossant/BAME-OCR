cd878b7c6b58bf7afe484fa853a2d6bc3433c724
antoine.lassagne <antoine.lassagne@epita.fr>
2023-11-23 17:03:40 +0100

    new start hough

--- src/ImageProcessing/ImageProcess.h
+++ src/ImageProcessing/ImageProcess.h
@@ -1,45 +1,47 @@
-#include <SDL2/SDL_surface.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void BlacknWhite(SDL_Surface* image);
+void BlacknWhite(SDL_Surface *image);
 
-void GreyScale(SDL_Surface* image);
+void GreyScale(SDL_Surface *image);
 
-void Contrast (SDL_Surface* image);
+void Contrast(SDL_Surface *image);
 
-void GaussianBlur (SDL_Surface* image);
+void GaussianBlur(SDL_Surface *image);
 
-void Bright (SDL_Surface* image);
+void Bright(SDL_Surface *image);
 
-void ArroundGaussianBlur (SDL_Surface* image);
+void ArroundGaussianBlur(SDL_Surface *image);
 
-SDL_Surface* Rotate (SDL_Surface* image, double angle);
+SDL_Surface *Rotate(SDL_Surface *image, double angle);
 
-SDL_Surface* Canny(SDL_Surface* image);
+SDL_Surface *Canny(SDL_Surface *image);
 
-SDL_Surface* Intensity_Gradian (SDL_Surface* image);
+SDL_Surface *Intensity_Gradian(SDL_Surface *image);
 
-SDL_Surface* Orientation_Gradian (SDL_Surface* image);
+SDL_Surface *Orientation_Gradian(SDL_Surface *image);
 
-Uint32 convolution(SDL_Surface* image, int x, int y, int* kernel, int kernelWidth);
+Uint32 convolution(SDL_Surface *image, int x, int y, int *kernel,
+                   int kernelWidth);
 
-SDL_Surface* Supp_Maxima(SDL_Surface* Intensity_Gradian_Image, SDL_Surface* Orientation_Gradian_Image);
+SDL_Surface *Supp_Maxima(SDL_Surface *Intensity_Gradian_Image,
+                         SDL_Surface *Orientation_Gradian_Image);
 
-void Thresholdhysteresis(SDL_Surface* image);
+void Thresholdhysteresis(SDL_Surface *image);
 
-Uint8 ComputeSeuil(SDL_Surface* image);
+Uint8 ComputeSeuil(SDL_Surface *image);
 
-SDL_Surface* hough_transform(SDL_Surface* image, int threshold, int state);
+SDL_Surface *hough_transform(SDL_Surface *image, int threshold, int state);
 
-SDL_Surface* new_hough_transform(SDL_Surface* image, int delta, int threshold);
+SDL_Surface *new_hough_transform(SDL_Surface *image, int delta, int threshold);
 
-void draw_line(SDL_Surface* image, int x1, int y1, int x2, int y2);
+void draw_line(SDL_Surface *image, int x1, int y1, int x2, int y2);
 
-void draw_hough_line(SDL_Surface* image, SDL_Surface* hough_pic, int seuil);
+void draw_hough_line(SDL_Surface *image, SDL_Surface *hough_pic, int seuil);
 
-void Auto_Rotate(SDL_Surface* image);
+void Auto_Rotate(SDL_Surface *image);
 
-void Crop(SDL_Surface* image, int x11, int y11, int x12, int y12, 
-        int x21, int y21, int x22, int y22 , char* filename);
+void Crop(SDL_Surface *image, int x11, int y11, int x12, int y12, int x21,
+          int y21, int x22, int y22, char *filename);
--- src/ImageProcessing/New_hough-transform.c
+++ src/ImageProcessing/New_hough-transform.c
@@ -8,66 +8,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-int** hough_matrix(SDL_Surface * image, int delta)
-{
-    int rho;
-    int R = sqrt(image->h*image->h+image->w*image->w);
-    int** Hough = (int**)malloc(R*sizeof(int**));
-    for (int i = 0; i < R; i++)
-        Hough[i] = (int*)calloc(180, sizeof(int*));
+SDL_Surface *new_hough_transform(SDL_Surface *image, int delta, int threshold) {
 
-    for (int x = 0; x < image->w; x++)
-    {
-        for (int y = 0; y < image->h; y++)
-        {
-            for(int tetha = 0; tetha < 180; tetha += delta)
-            {
-                rho = x * cos(tetha) + y * sin(tetha);
-                if (rho >= 0)
-                    Hough[rho][tetha] += 1;
-            }
-        }
-    }
-    return Hough;
-}
-
-SDL_Surface* draw_line2(SDL_Surface * image, int rho, int tetha)
-{
-    const SDL_PixelFormat* format = image->format;
-    Uint32* pixtab = image->pixels;
-    int y;
-    for (int x = 0; x < image->w; x++)
-    {
-        y = (rho - x * cos(tetha)) / sin(tetha);
-
-        if (y > image->h)
-            printf("%d\n", y);
-
-        if (y >= 0)
-            pixtab[y * image->w + x] = SDL_MapRGBA(format, 255, 0, 0, 255); // FIXME: seg fault
-    }
-
-    return image;
-}
-
-SDL_Surface* new_hough_transform(SDL_Surface * image, int delta, int threshold)
-{
-    int** Hought = hough_matrix(image, delta);
-
-    int R = sqrt(image->h*image->h+image->w*image->w);
-
-    for (int rho = 0; rho < R; rho++)
-    {
-        for (int tetha = 0; tetha < 180; tetha++)
-        {
-            if (Hought[rho][tetha] > threshold)
-            {
-                image = draw_line2(image, rho, tetha);
-            }
-        }
-    }
-
-    printf("ok\n");
-    
-    return image;
 }
