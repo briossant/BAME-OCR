80f822cac7fdcd0e4d578849b77d6028426af43f
Crosson Brieuc <brieuc.crosson@epita.fr>
2023-11-30 18:00:33 +0100

    separated hough and griddetection in parser + remove unused hough arguments

--- src/ImageProcessing/ImageProcess.h
+++ src/ImageProcessing/ImageProcess.h
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 typedef struct {
-    int x;
-    int y;
+  int x;
+  int y;
 } pair;
 
 void BlacknWhite(SDL_Surface *image);
@@ -40,8 +40,7 @@ Uint8 ComputeSeuil(SDL_Surface *image);
 
 SDL_Surface *hough_transform(SDL_Surface *image, int threshold, int state);
 
-int *new_hough_transform(SDL_Surface *image, int delta, int threshold,
-                         int *return_size);
+int *new_hough_transform(SDL_Surface *image, int *return_size);
 
 void draw_line(SDL_Surface *image, int x1, int y1, int x2, int y2,
                Uint32 color);
