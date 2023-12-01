5f4e21dcd96372da03b7b617d0d510ea01ff785d
Prog_up <prog_up@orange.fr>
2023-12-01 01:06:35 +0100

    Update BlacknWhite.c

--- src/ImageProcessing/BlacknWhite.c
+++ src/ImageProcessing/BlacknWhite.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void BlacknWhite(SDL_Surface *image) {
+void BlacknWhite(SDL_Surface *image)
+{
   Uint8 biais = 127;
 
   int height = image->h;
@@ -14,14 +15,18 @@ void BlacknWhite(SDL_Surface *image) {
   Uint32 *pixtab = image->pixels;
   Uint8 r, g, b, a;
 
-  for (int x = 0; x < width * height; x++) {
+  for (int x = 0; x < width * height; x++)
+  {
     SDL_GetRGBA(pixtab[x], format, &r, &g, &b, &a);
 
-    if ((r + g + b) / 3 > biais) {
+    if ((r + g + b) / 3 > biais)
+    {
       r = 255;
       g = 255;
       b = 255;
-    } else {
+    }
+    else
+    {
       r = 0;
       g = 0;
       b = 0;
@@ -32,14 +37,16 @@ void BlacknWhite(SDL_Surface *image) {
   }
 }
 
-void InvertColor(SDL_Surface *image) {
+void InvertColor(SDL_Surface *image)
+{
   int height = image->h;
   int width = image->w;
   const SDL_PixelFormat *format = image->format;
   Uint32 *pixtab = image->pixels;
   Uint8 r, g, b, a;
 
-  for (int x = 0; x < height * width; x++) {
+  for (int x = 0; x < height * width; x++)
+  {
     SDL_GetRGBA(pixtab[x], format, &r, &g, &b, &a);
 
     r = 255 - r;
@@ -51,7 +58,8 @@ void InvertColor(SDL_Surface *image) {
   }
 }
 
-void Balance(SDL_Surface *image) {
+void Balance(SDL_Surface *image)
+{
   // int min = 255;
   // int max = 0;
   // size_t count = 0;
@@ -69,28 +77,12 @@ void Balance(SDL_Surface *image) {
   int count_white = 0;
   int invert = 0;
 
-  // int ker[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-
-  for (int x = 0; x < height * width; x++) {
+  for (int x = 0; x < height * width; x++)
+  {
     SDL_GetRGBA(pixtab[x], format, &r, &g, &b, &a);
-    // convolution(image, x % width, x / width, ker, 3);
-    if (r > 50) {
+    if (r > 50)
       count_white++;
-    }
-    /*
-    if (r > max)
-      max = r;
-    if (r < min)
-      min = r;
-    count += r;
-    */
   }
-  /*
-      int middle = (max + min) / 3;
-      middle += count / (width * height);
-      middle /= 2;
-  */
-
   Uint8 threshold = ComputeSeuil(image_converted) / 4;
   // printf("%i\n", ComputeSeuil(image_converted) / 4);
   if (count_white >
@@ -104,7 +96,8 @@ void Balance(SDL_Surface *image) {
   queue[0] = 0;
   int coo = 0;
 
-  while (i_current < i_insert) {
+  while (i_current < i_insert)
+  {
     int x = queue[i_current] % width;
     int y = queue[i_current] / width;
     coo = queue[i_current];
@@ -112,25 +105,31 @@ void Balance(SDL_Surface *image) {
     Uint8 r, g, b, a, r2, g2, b2, a2;
     SDL_GetRGBA(pixtab[coo], format, &r, &g, &b, &a);
 
-    if (x + 1 < width) {
+    if (x + 1 < width)
+    {
       SDL_GetRGBA(pixtab[coo + 1], format, &r2, &g2, &b2, &a2);
-      if (r - r2 <= threshold && a2 != 254) {
+      if (r - r2 <= threshold && a2 != 254)
+      {
         queue[i_insert++] = coo + 1;
         pixtab[coo + 1] = SDL_MapRGBA(format, r2, g2, b2, 254);
       }
     }
 
-    if (x - 1 >= 0) {
+    if (x - 1 >= 0)
+    {
       SDL_GetRGBA(pixtab[coo - 1], format, &r2, &g2, &b2, &a2);
-      if (r - r2 <= threshold && a2 != 254) {
+      if (r - r2 <= threshold && a2 != 254)
+      {
         queue[i_insert++] = coo - 1;
         pixtab[coo - 1] = SDL_MapRGBA(format, r2, g2, b2, 254);
       }
     }
 
-    if (y + 1 < height) {
+    if (y + 1 < height)
+    {
       SDL_GetRGBA(pixtab[coo + width], format, &r2, &g2, &b2, &a2);
-      if (r - r2 <= threshold && a2 != 254) {
+      if (r - r2 <= threshold && a2 != 254)
+      {
         queue[i_insert++] = coo + width;
         pixtab[coo + width] = SDL_MapRGBA(format, r2, g2, b2, 254);
       }
@@ -139,24 +138,6 @@ void Balance(SDL_Surface *image) {
   }
   free(queue);
 
-  /*
-  for (int x = 0; x < height * width; x++) {
-    SDL_GetRGBA(pixtab[x], format, &r, &g, &b, &a);
-
-    if (invert && r > middle) {
-      r = 255;
-      g = 255;
-      b = 255;
-    } else if (!invert && r <= middle) {
-      r = 0;
-      g = 0;
-      b = 0;
-    }
-
-    // Pixel Update
-    pixtab[x] = SDL_MapRGBA(format, r, g, b, a);
-  }
-*/
   if (invert)
     InvertColor(image);
 }
