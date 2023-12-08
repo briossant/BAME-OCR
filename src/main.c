edddb42b476c38ebe013b502996a1914b374857a
Crosson Brieuc <crosson.brieuc@gmail.com>
2023-12-08 23:28:28 +0100

    repaired main.c

--- src/main.c
+++ src/main.c
@@ -1,20 +1,25 @@
 #include "ImageProcessing/ImageProcess.h"
+#include "Interface/interface.h"
 #include "NeuralNetwork/network/NeuralNetwork.h"
 #include "SudokuSolver/Sudoku_Solver.h"
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_surface.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         printf("Usage: ./ocr <image path> <network path (optional)>\n");
         return 1;
     }
     char *nn_path = DEFAULT_NN;
-    if (argc >= 3)
-    {
+    if (argc >= 3) {
         nn_path = argv[2];
     }
+    ThreadParameters *parameters = malloc(sizeof(ThreadParameters));
+    parameters->angle = 0;
+    parameters->auto_rotate = 1;
+    parameters->filename = argv[1];
+    parameters->filename_resolved = "Nimp.png";
+    BAME(parameters);
+    return 0;
 }
